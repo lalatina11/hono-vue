1e4d351acb3de892f2b34b4940541baba75b5154
lalatina11 <rahmadancandra@gmail.com>
2025-06-02 14:13:42 +0700

    chore: create navbar

---
+++ client/src/components/Nabar.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="flex justify-between items-center p-4">
+        <h1>EmBlogs</h1>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+        </nav>
+        <button>
+            Mode
+        </button>
+    </header>
+</template>
+<script lang="ts" setup>
+import { RouterLink } from "vue-router"
+</script>

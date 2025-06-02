b83b47bad7cf69c7f2a57e66589befe91384f5d4
lalatina11 <rahmadancandra@gmail.com>
2025-06-02 14:48:09 +0700

    refactor: rename navbar compt

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import ModeToggle from "./ModeToggle.vue";
+import NavDropDown from "./NavDropDown.vue";
+
+</script>
+<template>
+    <header class="flex justify-between items-center p-4 border-b">
+        <NavDropDown />
+        <RouterLink class="text-xl font-semibold" to="/">iBlogs</RouterLink>
+        <nav class="gap-2 items-center hidden md:flex">
+            <RouterLink class="btn btn-link" to="/">Home</RouterLink>
+            <RouterLink class="btn btn-link" to="/about">About</RouterLink>
+        </nav>
+        <ModeToggle class="hidden md:block" />
+    </header>
+</template>

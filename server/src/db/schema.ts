1cd03685611bb5dfab829d7f26e7613450fca6df
lalatina11 <rahmadancandra@gmail.com>
2025-06-02 15:23:38 +0700

    refactor: change user schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,8 +1,8 @@
 import { pgTable, varchar } from "drizzle-orm/pg-core";
 export const usersTable = pgTable('users', (t) => ({
     id: t.uuid().notNull().primaryKey().defaultRandom(),
-    email: varchar('email', { length: 254 }),
-    username: varchar('username', { length: 256, }),
+    email: varchar('email', { length: 254 }).notNull(),
+    username: varchar('username', { length: 256, }).notNull(),
     name: varchar('name', { length: 256, }),
     password: varchar('password', { length: 256 }),
     avatar: varchar('avatar', { length: 256 }),

03605c0563212039a41722a5c486bca67adc50b1
lalatina11 <rahmadancandra@gmail.com>
2025-06-02 15:21:45 +0700

    add: db connection pool

---
+++ server/src/db/index.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+const pool = new Pool({ database: process.env.DATABASE_URL })
+const db = drizzle(pool);
+export default db

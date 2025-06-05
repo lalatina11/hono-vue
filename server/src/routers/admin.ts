b67b9fce01aff5f8c082b295286a93e942571882
lalatina11 <rahmadancandra@gmail.com>
2025-06-05 19:52:55 +0700

    feat: add admin router

---
+++ server/src/routers/admin.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { adminController } from "../controller/admin.js";
+
+const router = new Hono()
+
+router.post("/request-add", adminController.requestAddAdmin)
+router.post("/create", adminController.create)
+router.post("/login", adminController.login)
+router.get("/current-admin", adminController.getLoggedAdmin)
+router.post("/logout", adminController.logout)
+
+export default router

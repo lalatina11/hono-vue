3f01e6167ec56fe2d14b7699603c19ce1867c75e
lalatina11 <rahmadancandra@gmail.com>
2025-06-05 20:04:56 +0700

    feat: add create expedition controller

--- server/src/controller/expeditions.ts
+++ server/src/controller/expeditions.ts
@@ -2,15 +2,18 @@ import type { Context } from "hono";
 import db from "../db/index.js";
 import { expeditionsTable, type Expeditions } from "../db/schema.js";
 import { getCookie } from "hono/cookie";
+import { adminRepository } from "../repository/admin.js";
 
 export const expeditionsController = {
     create: async (c: Context) => {
         try {
-            const token = await getCookie(c, 'admin_token') as string
+            const { adminId } = await adminRepository.getAdminByToken(c)
             const { title, desc } = await c.req.json() as Expeditions
-            const newExpedition = await db.insert(expeditionsTable).values({ adminId: token, desc, title }).returning()
+            if (title.trim().length < 1 || desc.trim().length < 1) {
+                throw new Error("Fill in the fields!")
+            }
+            const newExpedition = await db.insert(expeditionsTable).values({ adminId: adminId, desc, title }).returning()
             return c.json(await newExpedition, { status: 201 })
-
         } catch (error) {
             return c.json({ error: (error as Error).message })
         }

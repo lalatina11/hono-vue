3f547d5547657e2722d57d7411c27744165e798a
lalatina11 <rahmadancandra@gmail.com>
2025-06-05 19:52:17 +0700

    feat: adding admin and expedition schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,14 +1,24 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
-export const usersTable = pgTable('users', (t) => ({
-    id: t.uuid().notNull().primaryKey().defaultRandom(),
-    email: varchar('email', { length: 254 }).notNull(),
+import { createId } from "@paralleldrive/cuid2";
+import { integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const adminTable = pgTable('admin', (t) => ({
+    id: uuid().notNull().primaryKey().defaultRandom(),
     username: varchar('username', { length: 256, }).notNull(),
-    name: varchar('name', { length: 256, }),
-    password: varchar('password', { length: 256 }),
+    name: varchar('name', { length: 256, }).notNull(),
+    password: varchar('password', { length: 256 }).notNull(),
     avatar: varchar('avatar', { length: 256 }),
-    bio: varchar('bio', { length: 256 }),
     createdAt: t.timestamp({ mode: "date" }).defaultNow(),
     updatedAt: t.timestamp({ mode: "date" }).defaultNow().$onUpdate(() => new Date),
 }))
 
-export type User = typeof usersTable.$inferSelect
+export const expeditionsTable = pgTable('expeditions', () => ({
+    id: integer().notNull().primaryKey().generatedAlwaysAsIdentity(),
+    resi: varchar('resi', { length: 255 }).notNull().unique().$defaultFn(() => createId()),
+    adminId: uuid().notNull().references(() => adminTable.id),
+    title: varchar().notNull(),
+    desc: varchar().notNull(),
+}))
+
+
+export type Admin = typeof adminTable.$inferSelect
+export type Expeditions = typeof expeditionsTable.$inferSelect

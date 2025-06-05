56e9f84d73abead49be41fd9614bda1ce0d8316c
lalatina11 <rahmadancandra@gmail.com>
2025-06-05 19:59:12 +0700

    feat: create admin repository

---
+++ server/src/repository/admin.ts
@@ -0,0 +1,19 @@
+import { getCookie } from "hono/cookie"
+import jwt, { type JwtPayload } from "jsonwebtoken"
+import db from "../db/index.js"
+import { adminTable } from "../db/schema.js"
+import type { Context } from "hono"
+import { eq } from "drizzle-orm"
+
+export const adminRepository = {
+    getAdminByToken: async (c: Context) => {
+        const token = await getCookie(c, "admin_token")
+        if (!token) throw new Error("Token are required!")
+        const verifyToken = jwt.verify(token, process.env.SECRET_KEY || "") as JwtPayload
+        const adminId = verifyToken?.id
+        const existingAdmin = await db.select().from(adminTable).where(eq(adminTable.id, adminId))
+        if (!existingAdmin || !existingAdmin.length) throw new Error("Akun admin tidak dikenali")
+
+        return { adminId, adminData: existingAdmin[0] }
+    }
+}

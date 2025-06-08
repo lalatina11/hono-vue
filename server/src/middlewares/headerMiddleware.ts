7b91ef137010c228434f43cc7bcbad1ebec936db
lalatina11 <rahmadancandra@gmail.com>
2025-06-08 15:24:25 +0700

    refactor: adjust variable name and file name

--- server/src/middlewares/headerMiddleware.ts
+++ server/src/middlewares/headerMiddleware.ts
@@ -1,8 +1,8 @@
 import type { Context, Next } from "hono";
 
 const headerMiddleware = async (c: Context, next: Next) => {
-    const apiKey = c.req.header('x-api-key');
-    if (!apiKey || apiKey !== 'your_secret_api_key') {
+    const apiKey = c.req.header('api_key');
+    if (!apiKey || apiKey !== 'rahasia') {
         return c.json({ error: 'Invalid API key' }, 401);
     }
     await next();

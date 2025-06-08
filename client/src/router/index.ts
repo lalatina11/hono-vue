ba3600ad21041a7594d26adccbab514703b4b5fe
lalatina11 <rahmadancandra@gmail.com>
2025-06-08 14:57:25 +0700

    feat: add auth route page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AuthView from '@/views/AuthView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       name: 'about',
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: "/auth",
+      name: "auth",
+      component: AuthView
+    }
   ],
 })
 

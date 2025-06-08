b0b1e7b944622e6670b28e0299c7e6678ae59e6c
lalatina11 <rahmadancandra@gmail.com>
2025-06-08 14:56:16 +0700

    feat: add logger and dynamic port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,11 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import expRouter from "./routers/expeditions.js"
 import adminRouter from './routers/admin.js'
-
+import { logger } from 'hono/logger'
+import "dotenv/config"
 const app = new Hono()
+
+app.use(logger())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -14,7 +17,7 @@ app.route("/api/admin", adminRouter)
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: Number(process.env.PORT) || 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })

4e085cb3f509f92907fe30d22453dca2ef73b145
lalatina11 <rahmadancandra@gmail.com>
2025-06-08 14:56:40 +0700

    feat: add auth layout

---
+++ client/src/layouts/AuthLayout.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-full h-full flex justify-center items-center min-h-screen">
+        <slot />
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps < { title: string } > ()
+document.title = props.title
+</script>

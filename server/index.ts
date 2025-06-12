25c82dd7ee242373dd86d4b432906f13e4640952
lalatina11 <rahmadancandra@gmail.com>
2025-06-12 11:52:44 +0700

    feat: add cors middleware

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,12 @@ import expRouter from "./routers/expeditions.js"
 import adminRouter from './routers/admin.js'
 import { logger } from 'hono/logger'
 import "dotenv/config"
+import { cors } from 'hono/cors'
 //! test comment
 const app = new Hono()
 
 app.use(logger())
+app.use(cors({ origin: process.env.FRONTEND_URL || ["http://localhost:5173", "http://localhost:4173"], credentials: true }))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')

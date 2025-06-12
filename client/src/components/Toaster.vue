17d02e517166cea298b6c396990b5beffbc4b5d6
lalatina11 <rahmadancandra@gmail.com>
2025-06-12 11:50:52 +0700

    feat: create toaster compt

---
+++ client/src/components/Toaster.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="toastMessage" @click="clearToast" class="toast">
+        <div class="alert alert-info">
+            <span class="font-semibold">{{ toastMessage }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useToasterStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import { watch } from 'vue';
+
+const { toastMessage } = storeToRefs(useToasterStore())
+const { clearToast } = useToasterStore()
+
+
+
+watch(toastMessage, (newVal) => {
+    if (newVal) {
+        setTimeout(() => {
+            clearToast()
+        }, 3000);
+    }
+})
+
+</script>

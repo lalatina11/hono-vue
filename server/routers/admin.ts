aa0acb51d405c9abeb1112c4e4be30f904dc0af1
lalatina11 <rahmadancandra@gmail.com>
2025-06-12 11:51:22 +0700

    feat: add admin api routes for request add and request add logout

--- server/routers/admin.ts
+++ server/routers/admin.ts
@@ -4,6 +4,8 @@ import { adminController } from "../controller/admin.js";
 const router = new Hono()
 
 router.post("/request-add", adminController.requestAddAdmin)
+router.post("/request-add/logout", adminController.logoutReqAdmin)
+router.get("/request-add", adminController.getAdminAuthority)
 router.post("/create", adminController.create)
 router.post("/login", adminController.login)
 router.get("/current-admin", adminController.getLoggedAdmin)

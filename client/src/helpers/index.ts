68b3ea9ce69c46f3dd8377aad754f373ae593c3d
lalatina11 <rahmadancandra@gmail.com>
2025-06-13 06:41:07 +0700

    feat: add api request helper

---
+++ client/src/helpers/index.ts
@@ -0,0 +1,10 @@
+export const apiRequest = {
+    get: async (url: string) => {
+        const res = await fetch(url, { credentials: "include" })
+        return { res }
+    },
+    post: async (url: string, formBody: any) => {
+        const res = await fetch(url, { method: "POST", credentials: "include", body: JSON.stringify(formBody) })
+        return { res }
+    }
+}

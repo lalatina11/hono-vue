79ced02bc4df9b179408800c08ddaacb0fe07e22
lalatina11 <rahmadancandra@gmail.com>
2025-06-13 07:36:16 +0700

    feat: get is logged in admin and its validation

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <script setup lang="ts">
+import { apiRequest } from "@/helpers";
 import MainLayout from '@/layouts/MainLayout.vue';
-import { useAdminStore } from '@/stores';
+import { useAdminStore, useToasterStore } from '@/stores';
 import { storeToRefs } from 'pinia';
+import { onMounted, watch } from 'vue';
 const { admin } = storeToRefs(useAdminStore())
-if (!admin.value) {
-  location.replace("/auth?type=login")
+const { setAdmin, removeAdmin } = useAdminStore()
+const { setToast } = useToasterStore()
+const getAdmin = async () => {
+  const { res } = await apiRequest.get('/api/admin/current-admin')
+  if (!res.ok) {
+    return removeAdmin()
+  }
+  const result = await res.json()
+  if (result.error) {
+    return setToast(result.message)
+  }
+
+  if (!result.data) {
+    return removeAdmin()
+  }
+  setAdmin(result.data)
+}
+
+onMounted(async () => {
+  await getAdmin()
+})
+
+watch(admin, () => {
+  if (!admin.value) {
+    return location.replace("/auth?type=login")
+  }
+})
+const handleLogout = async () => {
+  try {
+    await apiRequest.post("/api/admin/logout")
+    removeAdmin()
+    setToast("Logout berhasil")
+    location.replace("/auth?type=login")
+  } catch (error) {
+    setToast((error as Error).message)
+  }
 }
 </script>
 
 <template>
   <MainLayout title="iExpress | Home">
     <main>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
+      <h1>{{ admin?.username }}</h1>
+      <form @submit.prevent="handleLogout">
+        <button class="btn btn-error">Logout</button>
+      </form>
     </main>
   </MainLayout>
 </template>

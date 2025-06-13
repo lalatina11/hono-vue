dfd5d0b446327fd7aa5a1928b1e58e379d099afd
lalatina11 <rahmadancandra@gmail.com>
2025-06-13 07:35:20 +0700

    refactor: api post not always require body

--- client/src/helpers/index.ts
+++ client/src/helpers/index.ts
@@ -3,7 +3,7 @@ export const apiRequest = {
         const res = await fetch(url, { credentials: "include" })
         return { res }
     },
-    post: async (url: string, formBody: any) => {
+    post: async (url: string, formBody?: any) => {
         const res = await fetch(url, { method: "POST", credentials: "include", body: JSON.stringify(formBody) })
         return { res }
     }

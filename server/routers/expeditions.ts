e24f0264ea10e917935476eeed73c9318d7459ae
lalatina11 <rahmadancandra@gmail.com>
2025-06-13 21:44:10 +0700

    feat: apply api router expeditions need

--- server/routers/expeditions.ts
+++ server/routers/expeditions.ts
@@ -3,6 +3,10 @@ import { expeditionsController } from "../controller/expeditions.js";
 
 const router = new Hono()
 
-router.post("/create", expeditionsController.create)
+router.post("/", expeditionsController.create)
+router.get("/", expeditionsController.read)
+router.get("/", expeditionsController.getExpById)
+router.post("/update", expeditionsController.update)
+router.post("/delete", expeditionsController.delete)
 
 export default router

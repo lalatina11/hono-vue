485bbfad16eaad72fead3037a990f100db9892fb
lalatina11 <rahmadancandra@gmail.com>
2025-06-14 18:51:02 +0700

    style : add style for simple dropdown nav

--- client/src/components/NavDropDown.vue
+++ client/src/components/NavDropDown.vue
@@ -1,16 +1,17 @@
 <template>
-    <details class="dropdown block md:hidden">
+    <details class="dropdown">
         <summary class="btn m-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="lucide lucide-chevron-down-icon lucide-chevron-down">
                 <path d="m6 9 6 6 6-6" />
             </svg></summary>
-        <ul class="menu dropdown-content bg-base-100 rounded-box z-1 min-w-32 p-2 shadow-sm">
-            <li class="w-fit">
-                <RouterLink to="/">Home</RouterLink>
+        <ul
+            class="menu dropdown-content bg-base-100 rounded-box z-1 min-w-32 p-2 shadow-sm flex flex-col justify-center items-center gap-3">
+            <li class="w-full">
+                <RouterLink class="btn" to="/">Home</RouterLink>
             </li>
-            <li class="w-fit">
-                <RouterLink to="/">About</RouterLink>
+            <li class="w-full">
+                <LogoutForm />
             </li>
             <li class="w-full">
                 <ModeToggle />
@@ -21,4 +22,6 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 import ModeToggle from './ModeToggle.vue';
+import LogoutForm from "@/components/auth/LogoutForm.vue"
+
 </script>
